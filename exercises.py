3a9d9af28c58b082ad22c733fd5253a7bcca5c66
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-08 17:16:21 -0400

    Complete exercise 6

--- exercises.py
+++ exercises.py
@@ -155,3 +155,28 @@ fav_colours.append('emerald')
 print(fav_colours)
 
 
+
+# ---------- EXERCISE SIX ---------
+
+# Make a new dictionary that contains a list of movies for each year. Each list of movies should be a list. Below is some data you can use.
+movies_by_year = {
+  1999 : ['The Matrix', 'Star Wars: Episode 1', 'The Mummy'],
+  2009 : ['Avatar', 'Star Trek', 'District 9']
+  2019 : ['How to Train Your Dragon 3', 'Toy Story 4', 'Star Wars: Episode 9']
+}
+
+# Make a new list that contains each row of the buttons on a phone. Each row should be a list.
+phone_buttons [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9],
+  ["*", 0, '#']
+]
+
+# Make a new list that contains information about three countries. Each country should be a dictionary that has a name, a continent, and whether or not it is an island.
+countries [
+  {'name': 'Canada', 'continent': 'North America', 'island': 'no'},
+  {'name': 'France', 'continent': 'Europe', 'island': 'no'},
+  {'name': 'New Zealand', 'continent': 'Oceania', 'island': 'yes'}
+]
+
