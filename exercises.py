db3ad3e3c16deff1ccba7990a03bcae8030879e5
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-08 15:41:53 -0400

    Complete exercise 2

--- exercises.py
+++ exercises.py
@@ -35,13 +35,45 @@ friend_ages = {
   'Brandon':27
 }
 
+
+
 # ---------- EXERCISE ONE ---------
 
+# Print out the list of coin flips.
 print(coin_flip)
 
+# Print out the first element of the list of your favourite colours.
 print(fav_colours[0])
 
+# Output the sorted version of the list of your friends and family members' ages.
+family_ages.sort()
+print(family_ages)
+
+# Add a new baby (0 years old) to the list of your family's ages.
 family_ages.append(0)
 # print(family_ages)
 
 print(movies['The Conjuring'])
+
+
+
+# ---------- EXERCISE TWO ---------
+
+# Print out the last element of the list of your favourite colours.
+print(fav_colours[-1])
+
+# Add a new city to the dictionary of cities and population.
+cities['Paris'] = 2.14
+# print(cities)
+
+# Reverse the list of coin flips and save it.
+coin_flip.reverse()
+# print(coin_flip)
+
+# Print out the population of one of the cities.
+print(cities['Paris'])
+
+# Print out a sentence about each item in the list of performing artists. For example:
+for band in fave_bands:
+  print(f"I think {band} is great.")
+
