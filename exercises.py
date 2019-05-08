e64a3be89d3edaa336f1dd037c9cd4dc9e1cf3d8
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-08 15:20:10 -0400

    Complete exercise 0

---
+++ exercises.py
@@ -0,0 +1,40 @@
+# ---------- EXERCISE ZERO ---------
+
+# --- Lists
+fav_colours = ['blue', 'teal', 'maroon']
+
+ages = [26, 23, 25, 29]
+
+coin_flip = ['heads', 'tails', 'heads', 'heads', 'tails']
+
+fave_bands = ['Kings of Leon', 'The Killers', 'Blink-182']
+
+# --- Dictionaries
+words = {
+  'nummet': 'light meal or luncheon',
+  'behoof': 'a benefit resulting from some course of action',
+  'stactometer': 'pipette with hollow bulb for counting drops'
+}
+
+movies = {
+  'Jaws': 1975,
+  'Empire Records': 1995,
+  'The Conjuring': 2013
+}
+
+cities = {
+  'Toronto': 2.93,
+  'Brooklyn': 2.64,
+  'Barcelona': 1.61
+}
+
+name_age = {
+  'Rachel': 23,
+  'Christina': 25,
+  'Alan': 29,
+  'Brandon':27
+}
+
+# ---------- EXERCISE ONE ---------
+
+print(coin_flip)
