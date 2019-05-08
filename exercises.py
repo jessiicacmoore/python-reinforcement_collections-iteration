7acf7329d0c938a0d19753db8b4c34473391582d
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-08 16:12:41 -0400

    Complete exercise 3

--- exercises.py
+++ exercises.py
@@ -77,3 +77,29 @@ print(cities['Paris'])
 for band in fave_bands:
   print(f"I think {band} is great.")
 
+
+
+# ---------- EXERCISE THREE ---------
+
+# Print out the first two performing artists in that list.
+print(fave_bands[0:2])
+
+# For each of your favourite movies, print out a sentence about when the movie was released.
+for movie, year in movies.items():
+  print(f"{movie} came out in {year}")
+
+# Sort and reverse the list of ages of your family. Save it and print it to the screen.
+family_ages.sort(reverse=True)
+print(family_ages)
+
+# Add "Beauty and the Beast" movie to your dictionary of movies information, but with a twist: the movie was released both in 1991 and in 2017. Print it out.
+movies['Beauty and the Beast'] = {
+  'original': 1991,
+  'remake': 2017
+}
+print(movies)
+
+
+
+
+
