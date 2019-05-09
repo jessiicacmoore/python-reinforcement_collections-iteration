37e0c207bbf59af4a73801a8f015cfecc91f4dfb
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-08 22:56:49 -0400

    Complete exercise 11

---
+++ exercises_part3.py
@@ -0,0 +1,8 @@
+# ---------- EXERCISE ELEVEN ---------
+for number in range(10):
+  if number % 3 == 0 and number % 5 == 0:
+    print ('BitMaker')
+  elif number % 3 == 0:
+    print('Bit')
+  elif number % 5 ==0:
+    print('Maker')
