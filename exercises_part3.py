dd303979b61409813926ceb93cc95a0e81e1cf7b
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-08 23:19:39 -0400

    Complete exercise 12

--- exercises_part3.py
+++ exercises_part3.py
@@ -1,8 +1,27 @@
 # ---------- EXERCISE ELEVEN ---------
-for number in range(10):
+for number in range(100):
   if number % 3 == 0 and number % 5 == 0:
     print ('BitMaker')
   elif number % 3 == 0:
     print('Bit')
   elif number % 5 ==0:
     print('Maker')
+
+# ---------- EXERCISE ELEVEN ---------
+pizzas = {}
+pizza_id = 0
+
+print('How many pizzas do you want to order?')
+pizza_qty = int(input())
+
+for pizza in range(pizza_qty):
+  pizza_id += 1
+  print("How many toppings for Pizza {}".format(pizza_id))
+  topping_qty = int(input())
+
+  pizzas['pizza {}'.format(pizza_id)] = topping_qty
+
+
+
+print(pizzas)
+  
